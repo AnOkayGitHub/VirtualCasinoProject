82b852ba236dcce747e2f14d9fafa87e968ed23e
Brandon Peter <brandonpeter99@yahoo.com>
2021-04-27 19:43:29 -0400

    We have comments now :p

--- VirtualCasino/src/BlackjackMenu.java
+++ VirtualCasino/src/BlackjackMenu.java
@@ -22,6 +22,11 @@ public class BlackjackMenu extends Menu {
 		playGame();
 	}
 		
+	/*
+	 * Condition for winning blackjack
+	 * Combines users current money with the money the user has just won
+	 * Updates the users total amount and asks if they would like to play again
+	 */
 	public void winGame(int winnings) throws InterruptedException {
 		Casino.setPlayerMoney(money + winnings);
 		
@@ -31,12 +36,15 @@ public class BlackjackMenu extends Menu {
 		userPrint("Dealer", "Do you want to play again? (Yes / No)");
 		String choice = InputManager.getChoiceFromUser(new String[] {"YES", "NO"});
 		
+		// If user enters yes for a rematch it goes back to replay the game
 		if(choice.toUpperCase().compareTo("YES") == 0) {
 			clear();
 			printBanner("Casino - Blackjack Table");
 			userPrint("Dealer", "So you want a rematch, huh?!");
 			sleep(3000);
 			goTo("Blackjack");
+			
+			// If user enters no it will return you to the main menu
 		} else {
 			clear();
 			printBanner("Casino - Blackjack Table");
@@ -47,6 +55,11 @@ public class BlackjackMenu extends Menu {
 		
 	}
 	
+	/*
+	 * Condition for losing blackjack
+	 * Subtracts users current money with the amount they bet on the game
+	 * Updates the users total amount and asks if they would like to play again
+	 */
 	public void loseGame(int losingAmount) throws InterruptedException {
 		Casino.setPlayerMoney(money - losingAmount);
 		
@@ -56,12 +69,15 @@ public class BlackjackMenu extends Menu {
 		userPrint("Dealer", "Do you want to play again? (Yes / No)");
 		String choice = InputManager.getChoiceFromUser(new String[] {"YES", "NO"});
 		
+		// If user enters yes for a rematch it goes back to replay the game
 		if(choice.toUpperCase().compareTo("YES") == 0) {
 			clear();
 			printBanner("Casino - Blackjack Table");
 			userPrint("Dealer", "So you want a rematch, huh?!");
 			sleep(3000);
 			goTo("Blackjack");
+			
+			// If user enters no it will return you to the main menu
 		} else {
 			clear();
 			printBanner("Casino - Blackjack Table");
@@ -74,14 +90,18 @@ public class BlackjackMenu extends Menu {
 	private void playGame() throws InterruptedException {
 		money = Casino.getPlayerMoney();
 		
+		// First two cards are randomly generated and then assigned to an int that combines them
 		int card1 = (int) (1 + Math.random() * 10);
 	    int card2 = (int) (1 + Math.random() * 10);
 	    int cardTotal = card1 + card2;
 	    
 	    clear();
 	    printBanner("Casino - Blackjack Table");
+	    
+	    //Shows user their total money and asks how much they want to bet
 		userPrint("Dealer", String.format("How much money would you like to bet?\nYou have $%s.", money));
 		
+		//Recieves user bet
 		int betAmount = InputManager.getIntegerFromUser(money);
 		
 		clear();
@@ -89,12 +109,18 @@ public class BlackjackMenu extends Menu {
 		userPrint("Dealer", "Bets have been placed!");
 		sleep(3000);
 			
+		/*if the users cards are less than 21 the dealer asks if they would like to draw another card
+		* or if they would like to stand
+		*/
 		if (cardTotal < 21) {	
 			clear();
 			printBanner("Casino - Blackjack Table");
 			userPrint("Dealer", "What do you want to do? (Hit or Stand)\nCurrent hand: " + cardTotal + ".");
 			
+			//Recieves user input if they would like to draw another card or stand
 			String response = InputManager.getChoiceFromUser(new String[] { "HIT", "STAND" });
+			
+			// If the user chose to hit they get another randomly generated card
 			if (response.toUpperCase().equals("HIT")) {
 				int card3 = (int) (1 + Math.random() * 10);
 				cardTotal += card3;
@@ -108,29 +134,37 @@ public class BlackjackMenu extends Menu {
 			    int dCard2 = (int) (1 + Math.random() * 10);
 			    int dTotal = dCard1 + dCard2;
 			    
+			    // If the dealer's cards are under 17 they draw a third card 
 			    while(dTotal < 17) {
 			    	int dCard3 = (int) (1 + Math.random() * 10);
 			    	dTotal += dCard3;
 			    }
 			    
+			    //Condition for just the Dealer busting and user winning the game
 			    if(dTotal > 21 && cardTotal < 21) {
 			    	clear();
 					printBanner("Casino - Blackjack Table");
 					userPrint("Dealer", "I Busted! I had " + dTotal + "!");
 					sleep(3000);
 					winGame(betAmount );
+					
+				    //Condition for the user winning by beating the dealer with no one busting
 			    } else if(cardTotal > dTotal) {
 			    	clear();
 					printBanner("Casino - Blackjack Table");
 					userPrint("Dealer", "I had " + dTotal + "!");
 					sleep(3000);
 					winGame(betAmount );
+					
+					 //Condition for the dealer winning by beating the user with no one busting
 			    } else if(cardTotal < dTotal) {
 			    	clear();
 					printBanner("Casino - Blackjack Table");
 					userPrint("Dealer", "I had " + dTotal + "!");
 					sleep(3000);
 					loseGame(betAmount );
+					
+					//Condition for the user and the dealer getting the same number which results in a tie
 			    } else {
 			    	clear();
 					printBanner("Casino - Blackjack Table");
@@ -139,6 +173,7 @@ public class BlackjackMenu extends Menu {
 				return;
 			}
 		
+			// Condition for losing the game by going over 21 
 		} else if (cardTotal > 21) {
 			clear();
 			printBanner("Casino - Blackjack Table");
@@ -146,6 +181,7 @@ public class BlackjackMenu extends Menu {
 			loseGame(betAmount );
 			return;
 			
+			// Condition for winning and getting blackjack
 		} else if (cardTotal == 21) {
 			clear();
 			printBanner("Casino - Blackjack Table");
