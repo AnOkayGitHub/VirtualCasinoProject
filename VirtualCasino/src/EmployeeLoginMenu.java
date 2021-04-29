fce8290b926775b27373f9251b2af3e1e94e8155
Brandon Peter <brandonpeter99@yahoo.com>
2021-04-29 17:02:49 -0400

    ELM comments added

--- VirtualCasino/src/EmployeeLoginMenu.java
+++ VirtualCasino/src/EmployeeLoginMenu.java
@@ -1,23 +1,29 @@
 
+// Declaring arrays for accepted usernames, Employee ID's and Employee pins
+// Also sets whether or not an employee is logged in or not. By default no one is logged in
 public class EmployeeLoginMenu extends Menu {
 	String[] ids = new String[] { "1234", "0000", "9876", "8675" };
 	String[] pins = new String[] { "1234", "1111", "5432", "3099" };
-	String[] usernames = new String[] { "Nick", "Brandom", "Matt", "Ryan" };
+	String[] usernames = new String[] { "Nick", "Brandon", "Matt", "Ryan" };
 	private boolean isLoggedIn = false;
 	
 	protected EmployeeLoginMenu(String id) {
 		super(id);
 	}
 	
+	
 	@Override
 	public void display() throws InterruptedException {
 		clear();
+		//Asks the user for their user ID and their pin then will accept user input
 		printBanner("Casino - Employee Login Terminal");
 		userPrint("Employee Portal", "Please enter your user ID, then your PIN, below.");
 		
+		//Takes employee ID and pin from user input
 		String uid = InputManager.getChoiceFromUser(4);
 		String pin = InputManager.getChoiceFromUser(4);
 		
+		// If the user does not provide correct credentials it will ask them to try again
 		while(!validate(uid, pin)) {
 			userPrint("Terminal", "Invalid Login. Please try again.");
 			sleep(1500);	
@@ -25,6 +31,7 @@ public class EmployeeLoginMenu extends Menu {
 			printBanner("Casino - Employee Login Terminal");
 			userPrint("Employee Portal", "Please enter your user ID, then your PIN, below.");
 			
+			//Takes employee ID and pin from user input
 			uid = InputManager.getChoiceFromUser(4);
 			pin = InputManager.getChoiceFromUser(4);
 			
@@ -33,6 +40,7 @@ public class EmployeeLoginMenu extends Menu {
 		goTo("EMenu");
 	}
 	
+	//Will set the system to logged in once the employee has entered their information in correctly
 	private boolean validate(String id, String p) throws InterruptedException {
 		isLoggedIn = false;
 		for(int i = 0; i < ids.length; i++) {
